0b3931c5c536022b7da0e79f91f04500a6011bb0
badvik83 <152312982+badvik83@users.noreply.github.com>
2024-10-05 21:41:31 -0400

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(a, b, c) {
+  
+    let newArray = new Array(a.length-2);
+    let newArrayCount = 0;
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] == b || a[i] == c)
+        {
+          continue;   
+        }
+      else {
+        newArray[newArrayCount] = a[i];
+        newArrayCount++;
+      }
+    }
+    return(newArray);
+  };  
+
+  console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
