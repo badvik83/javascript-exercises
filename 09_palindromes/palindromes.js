6bd61b3a6fbb2a416ef07f828b1b0d3589fb123f
badvik83 <152312982+badvik83@users.noreply.github.com>
2024-11-03 17:58:51 -0500

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
 const palindromes = function () {
+    let word = prompt("Enter a word to check if it's a palindrome");
+    word = word.toLowerCase().match(/[a-z0-9]/g).join("");
+    let reversedWord = word.split("").reverse().join("");
 
+    return word === reversedWord;
 };
 
 // Do not edit below this line
