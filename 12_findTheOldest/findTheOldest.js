973eb118402ddff4805cd5336cbc7d0d58bd6752
badvik83 <152312982+badvik83@users.noreply.github.com>
2024-11-03 21:20:07 -0500

    two options for my own records

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,44 @@
 const findTheOldest = function() {
 
+  //   let oldestAge = 0;
+//   let oldestPerson = [];
+
+//   people.map((a) => {
+//     if(a.yearOfDeath - a.yearOfBirth > oldestAge) {
+//         oldestAge = a.yearOfDeath - a.yearOfBirth;
+//         oldestPerson = a;
+//     }
+//     else if (!a.yearOfDeath) {
+//         let currentAge = new Date().getFullYear();
+//         if(currentAge - a.yearOfBirth > oldestAge) {
+//             oldestAge = currentAge - a.yearOfBirth;
+//             oldestPerson = a;
+//         }
+//     }
+//   });
+
+// console.log(oldestPerson);
+
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
+  
+  console.log(findTheOldest(people));
+  
 };
 
 // Do not edit below this line
