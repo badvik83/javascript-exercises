dc0f5f9f3a3b547d83e13ff029172e3fa64b7ff8
badvik83 <152312982+badvik83@users.noreply.github.com>
2024-11-03 19:49:36 -0500

    Modified to optimize memory alloc
    Doesn't work with the original Task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
 const fibonacci = function() {
 
+  // Modified to use memory only up to the certain request
+  function fibonacciSeq (fibSeqPos) {
+    let a = 1;
+    let b = 1;
+    let theResult = 0;
+    for (let i = 0; i < fibSeqPos; i++) {
+        theResult = a;
+        let previousA = a;
+        a = b;
+        b = previousA + b;
+    }
+    return theResult;
+};
+
+alert(fibonacciSeq(10));
+  
 };
 
 // Do not edit below this line
