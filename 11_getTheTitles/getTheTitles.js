a4ad94ad69532acd22a6824e5287029d02db9625
badvik83 <152312982+badvik83@users.noreply.github.com>
2024-11-03 20:06:35 -0500

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
 const getTheTitles = function() {
-
+let titlesArr = [];
+    for (const book in books) {
+        titlesArr.push(books.title);
+    }
+  return titlesArr;
 };
 
 // Do not edit below this line
